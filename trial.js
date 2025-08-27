04734659c127aa4074fd72501e13a0ea0a40e3ce
AnujDubeyy <i.anujdubey@gmail.com>
2025-08-27 17:42:01 +0530

    adding main and trial js file

---
+++ trial.js
@@ -0,0 +1,11 @@
+// Example: Calculate the sum of numbers in an array
+
+const numbers = [10, 20, 30, 40, 50];
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+
+console.log("The sum is:", sum);
